7882abdfd282a6237ae76ba7a1bfd3906bd96a13
Aditbise <aditbisht222@gmail.com>
2025-07-15 11:11:23 +0530

    created register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,44 @@
-import { useState } from 'react';
+import { useState } from "react";
+import axios from "axios"; // ✅ Import axios
 
 function App() {
-  const [link, setLink] = useState("");
+  const [registered, setRegistered] = useState(false); // ✅ Boolean instead of string
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        "http://20.244.56.144/evaluation-service/register",
+        {
+          email: "aditbisht24@gmail.com",
+          name: "Aditya Bisht",
+          mobileNo: "9917522589",
+          githubUsername: "Aditbise",
+          rollNo: "218203",
+          accessCode: "QAhDUr" 
+        }
+      );
 
+      localStorage.setItem("clientID", response.data.clientID);
+      localStorage.setItem("clientSecret", response.data.clientSecret);
+
+      alert("✅ Registered! ClientID & Secret saved.");
+      setRegistered(true);
+    } catch (err) {
+      console.error("Registration fail");
+    }
   };
 
   return (
     <>
       <h1>URL Shortener</h1>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input 
-            type="text" 
-            placeholder="Enter link"
-            value={link}
-            onChange={(e) => setLink(e.target.value)}
-          />
-          <button type="submit">Shorten</button>
-        </form>
+      <div style={{ padding: "20px" }}>
+        <h1>Candidate Registration</h1>
+
+        {!registered ? ( 
+          <button onClick={handleSubmit}>Register Now</button>
+        ) : (
+          <p>Registered</p>
+        )}
       </div>
     </>
   );
